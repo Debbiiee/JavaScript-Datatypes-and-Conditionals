3cd36c5e7c7cd7ad0bd26c9173be961135305800
debbyrose <87522669+debbyrose@users.noreply.github.com>
2021-10-25 20:38:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+/*1. To convert Celsius to Fahrenheit, you can use the following formula:
+F=C×1.8+32
+Directions: Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12°C. NOTE: "12°C" reads as "12 degrees Celsius".
+*/
+
+let celsius = 12;
+let fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+
+//2. Favourite Food:
+
+let favouriteFood = "Fried rice"
+console.log(favouriteFood);
+
+
+//3. String Equality
+
+const answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
+
+
+//4. All tied up
+
+var joke = "Why couldn't the shoes go out and play?\n They were all \"tied\" up!"; 
+console.log(joke);
+
+
+
+//5. Yosa Bosun
+
+let haiku = "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east.";
+console.log(haiku);
+
+
+//6. What’s my name?
+
+var fullName = "Olukanni Oluwapemi";
+console.log(fullName);
+
+//7. Out to Dinner
+
+let bill = 10.25 + 3.99 + 7.15; 
+let tip = bill * 0.15;
+let total = bill + tip;
+console.log("$" + total.toFixed(2));
+
+
+//8. Mad Libs
+
+let adjective1 = "amazing"; 
+let adjective2 = "fun";
+let adjective3 = "entertaining";
+let madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madLib);
+
+
+//9. One Awesome message
+
+let firstName = "Pemi";
+let interest = "novels";
+let hobby = "watch movies";
+let awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage);
+
+
